940e9baa41674d5a272bfad2a55f53a86d518b51
MatteoS <matteo.signer@gmail.com>
2015-08-08 14:43:56 +0200

    Correct detection of GLSL version for curve rendering

--- src/itdelatrisu/opsu/objects/curves/Curve.java
+++ src/itdelatrisu/opsu/objects/curves/Curve.java
@@ -87,7 +87,7 @@ public abstract class Curve {
 		Curve.borderColor = borderColor;
 
 		ContextCapabilities capabilities = GLContext.getCapabilities();
-		mmsliderSupported = capabilities.GL_EXT_framebuffer_object && capabilities.OpenGL30;
+		mmsliderSupported = capabilities.GL_EXT_framebuffer_object && capabilities.OpenGL33;
 		if (mmsliderSupported)
 			CurveRenderState.init(width, height, circleSize);
 		else {
