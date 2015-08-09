8173b9b4c2c0e522e5ac52819408924702bba263
Jeffrey Han <itdelatrisu@gmail.com>
2015-08-08 20:34:49 -0500

    Added Mnetwork download mirror. URL: http://osu.uu.gl/
    Note that there's no API available, so this just parses the HTML for beatmap data.  It's pretty slow compared to the other servers.
    
    Signed-off-by: Jeffrey Han <itdelatrisu@gmail.com>

--- src/itdelatrisu/opsu/states/DownloadsMenu.java
+++ src/itdelatrisu/opsu/states/DownloadsMenu.java
@@ -35,6 +35,7 @@ import itdelatrisu.opsu.downloads.DownloadNode;
 import itdelatrisu.opsu.downloads.servers.BloodcatServer;
 import itdelatrisu.opsu.downloads.servers.DownloadServer;
 import itdelatrisu.opsu.downloads.servers.HexideServer;
+import itdelatrisu.opsu.downloads.servers.MnetworkServer;
 import itdelatrisu.opsu.downloads.servers.YaSOnlineServer;
 import itdelatrisu.opsu.ui.MenuButton;
 import itdelatrisu.opsu.ui.UI;
@@ -74,7 +75,9 @@ public class DownloadsMenu extends BasicGameState {
 	private static final int MIN_REQUEST_INTERVAL = 300;
 
 	/** Available beatmap download servers. */
-	private static final DownloadServer[] SERVERS = { new BloodcatServer(), new HexideServer(), new YaSOnlineServer() };
+	private static final DownloadServer[] SERVERS = {
+		new BloodcatServer(), new HexideServer(), new YaSOnlineServer(), new MnetworkServer()
+	};
 
 	/** The beatmap download server index. */
 	private int serverIndex = 0;
