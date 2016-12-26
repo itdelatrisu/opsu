97425aef198e60ff318c1cee42cf95b9b0a310b4
Jeffrey Han <itdelatrisu@gmail.com>
2016-12-26 14:37:32 -0500

    Updated license headers and added missing ones.

--- src/org/newdawn/slick/state/transition/EasedFadeOutTransition.java
+++ src/org/newdawn/slick/state/transition/EasedFadeOutTransition.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2013, Slick2D
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the Slick2D nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.newdawn.slick.state.transition;
 
 import itdelatrisu.opsu.ui.animations.AnimationEquation;
@@ -30,14 +58,14 @@ public class EasedFadeOutTransition implements Transition {
 
 	/**
 	 * Create a new eased fade out transition.
-	 * 
+	 *
 	 * @param color The color we're going to fade out to
 	 */
 	public EasedFadeOutTransition(Color color) { this(color, 500, AnimationEquation.OUT_QUART); }
 
 	/**
 	 * Create a new eased fade out transition.
-	 * 
+	 *
 	 * @param color The color we're going to fade out to
 	 * @param fadeTime The time it takes the fade to occur
 	 */
@@ -45,7 +73,7 @@ public class EasedFadeOutTransition implements Transition {
 
 	/**
 	 * Create a new eased fade out transition.
-	 * 
+	 *
 	 * @param color The color we're going to fade out to
 	 * @param fadeTime The time it takes the fade to occur
 	 * @param eq The easing function to use
