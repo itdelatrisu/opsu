04962a06d4692de0f6602466c68e1daa284fdf28
Jeffrey Han <itdelatrisu@gmail.com>
2017-01-09 01:11:20 -0500

    Updated copyright notices.
    Signed-off-by: Jeffrey Han <itdelatrisu@gmail.com>

--- src/itdelatrisu/opsu/OptionGroup.java
+++ src/itdelatrisu/opsu/OptionGroup.java
@@ -1,6 +1,6 @@
 /*
  * opsu! - an open-source osu! client
- * Copyright (C) 2014, 2015, 2016 Jeffrey Han
+ * Copyright (C) 2014-2017 Jeffrey Han
  *
  * opsu! is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
