588d235cff2a1dfbe1f79b0d748e74ee5c9b9caa
yugecin <yugecin@users.noreply.github.com>
2017-01-29 22:01:03 +0100

    fix kinetic scrolling issue

--- src/itdelatrisu/opsu/ui/KineticScrolling.java
+++ src/itdelatrisu/opsu/ui/KineticScrolling.java
@@ -127,7 +127,8 @@ public class KineticScrolling {
 				amplitude = 0;
 				target = position = max;
 			}
-		} else if (position < min) {
+		}
+		if (position < min) {
 			if (allowOverScroll) {
 				scrollToPosition(min);
 			} else {
