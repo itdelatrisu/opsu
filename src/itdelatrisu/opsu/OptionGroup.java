fdf677b9db8e54ca3b05a22efc7c64227fe923cc
yugecin <yugecin@users.noreply.github.com>
2017-01-29 13:59:52 +0100

    Attempt to make logical groups

--- src/itdelatrisu/opsu/OptionGroup.java
+++ src/itdelatrisu/opsu/OptionGroup.java
@@ -25,59 +25,84 @@ import itdelatrisu.opsu.Options.GameOption;
  */
 public class OptionGroup {
 	/** All option groups. */
-	public static final OptionGroup[] ALL_OPTIONS = new OptionGroup[]{
-		new OptionGroup("Display", new GameOption[]{
+	public static final OptionGroup[] ALL_OPTIONS = new OptionGroup[] {
+		new OptionGroup("GENERAL", null),
+		new OptionGroup("GENERAL", new GameOption[]{
+			GameOption.DISABLE_UPDATER,
+			GameOption.ENABLE_WATCH_SERVICE,
+		}),
+		new OptionGroup("LANGUAGE", new GameOption[] {
+			GameOption.SHOW_UNICODE,
+		}),
+		new OptionGroup("GRAPHICS", null),
+		new OptionGroup("RENDERER", new GameOption[]{
 			GameOption.SCREEN_RESOLUTION,
 			GameOption.FULLSCREEN,
-			GameOption.SKIN,
 			GameOption.TARGET_FPS,
 			GameOption.SHOW_FPS,
-			GameOption.SHOW_UNICODE,
 			GameOption.SCREENSHOT_FORMAT,
+		}),
+		new OptionGroup("SLIDERS", new GameOption[] {
+			GameOption.SNAKING_SLIDERS,
+		}),
+		new OptionGroup("MISC", new GameOption[] {
 			GameOption.DYNAMIC_BACKGROUND,
-			GameOption.LOAD_HD_IMAGES,
-			GameOption.LOAD_VERBOSE,
 			GameOption.PARALLAX
 		}),
-		new OptionGroup("Music", new GameOption[] {
+		new OptionGroup("SKIN", null),
+		new OptionGroup("SKIN", new GameOption[]{
+			GameOption.SKIN,
+			GameOption.LOAD_HD_IMAGES,
+			GameOption.LOAD_VERBOSE,
+			GameOption.IGNORE_BEATMAP_SKINS,
+		}),
+		new OptionGroup("CURSOR", new GameOption[] {
+			GameOption.CURSOR_SIZE,
+			GameOption.DISABLE_CURSOR
+		}),
+		new OptionGroup("AUDIO", null),
+		new OptionGroup("VOLUME", new GameOption[]{
 			GameOption.MASTER_VOLUME,
 			GameOption.MUSIC_VOLUME,
 			GameOption.EFFECT_VOLUME,
 			GameOption.HITSOUND_VOLUME,
+		}),
+		new OptionGroup("MISC", new GameOption[] {
 			GameOption.MUSIC_OFFSET,
 			GameOption.DISABLE_SOUNDS,
 			GameOption.ENABLE_THEME_SONG
 		}),
-		new OptionGroup("Gameplay", new GameOption[] {
+		new OptionGroup("GAMEPLAY", null),
+		new OptionGroup("GENERAL", new GameOption[] {
 			GameOption.BACKGROUND_DIM,
 			GameOption.FORCE_DEFAULT_PLAYFIELD,
 			GameOption.ENABLE_VIDEOS,
-			GameOption.IGNORE_BEATMAP_SKINS,
-			GameOption.SNAKING_SLIDERS,
 			GameOption.SHOW_HIT_LIGHTING,
 			GameOption.SHOW_COMBO_BURSTS,
 			GameOption.SHOW_PERFECT_HIT,
 			GameOption.SHOW_FOLLOW_POINTS,
 			GameOption.SHOW_HIT_ERROR_BAR
 		}),
-		new OptionGroup("Input", new GameOption[] {
+		new OptionGroup("INPUT", null),
+		new OptionGroup("KEY MAPPING", new GameOption[] {
 			GameOption.KEY_LEFT,
 			GameOption.KEY_RIGHT,
+		}),
+		new OptionGroup("MOUSE", new GameOption[] {
 			GameOption.DISABLE_MOUSE_WHEEL,
 			GameOption.DISABLE_MOUSE_BUTTONS,
-			GameOption.CURSOR_SIZE,
-			GameOption.DISABLE_CURSOR
 		}),
-		new OptionGroup("Custom", new GameOption[] {
+		new OptionGroup("CUSTOM", null),
+		new OptionGroup("DIFFICULTY", new GameOption[] {
 			GameOption.FIXED_CS,
 			GameOption.FIXED_HP,
 			GameOption.FIXED_AR,
 			GameOption.FIXED_OD,
+		}),
+		new OptionGroup("MISC", new GameOption[] {
 			GameOption.CHECKPOINT,
 			GameOption.REPLAY_SEEKING,
-			GameOption.DISABLE_UPDATER,
-			GameOption.ENABLE_WATCH_SERVICE
-		})
+		}),
 	};
 
 	/** The category name. */
